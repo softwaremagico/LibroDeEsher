65b7a2f1acc6dddc554893d75499c5eedd04d26d
Jorge Hortelano <jhortelano@biit-solutions.com>
2014-08-01 09:19:34 +0200

    Pdf Test added.

---
+++ librodeesher-core/src/test/java/com/softwaremagico/librodeesher/pj/CharacterCreationTest.java
@@ -0,0 +1,42 @@
+package com.softwaremagico.librodeesher.pj;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.softwaremagico.librodeesher.pj.pdf.PdfCombinedSheet;
+import com.softwaremagico.librodeesher.pj.pdf.PdfStandardSheet;
+import com.softwaremagico.librodeesher.pj.random.RandomCharacterPlayer;
+import com.softwaremagico.librodeesher.pj.skills.SkillFactory;
+import com.softwaremagico.persistence.dao.hibernate.CharacterPlayerDao;
+
+@Test
+public class CharacterCreationTest {
+	private final static String PDF_PATH = System.getProperty("java.io.tmpdir") + "/test.pdf";
+	private CharacterPlayerDao characterPlayerDao = CharacterPlayerDao.getInstance();
+	private CharacterPlayer characterPlayer;
+
+	@Test(groups = { "characterCreation" })
+	public void createCharacter() {
+		characterPlayer = new RandomCharacterPlayer(null, null, null, null, 1).getCharacterPlayer();
+
+	}
+
+	@Test(groups = { "characterStorage" }, dependsOnMethods = { "createCharacter" })
+	public void storeCharacter() {
+		characterPlayerDao.makePersistent(characterPlayer);
+		Assert.assertNotNull(characterPlayer.getId());
+		Assert.assertNotNull(characterPlayer.getTotalValue(SkillFactory.getAvailableSkill("Trepar")));
+		Assert.assertTrue(characterPlayer.getRemainingDevelopmentPoints() >= 0);
+	}
+
+	@Test(groups = { "characterPdf" }, dependsOnMethods = { "storeCharacter" })
+	public void standardPdf() throws Exception {
+		new PdfStandardSheet(characterPlayer, PDF_PATH, false);
+	}
+
+	@Test(groups = { "characterPdf" }, dependsOnMethods = { "storeCharacter" })
+	public void combinedPdf() throws Exception {
+		new PdfCombinedSheet(characterPlayer, PDF_PATH);
+	}
+
+}
