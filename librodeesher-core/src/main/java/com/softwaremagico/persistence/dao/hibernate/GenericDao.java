99391dad3a314d6377b1c8f5ab609ef7740f3146
Jorge Hortelano <jhortelano@biit-solutions.com>
2014-08-13 09:49:02 +0200

    Solved getAll issue.

--- librodeesher-core/src/main/java/com/softwaremagico/persistence/dao/hibernate/GenericDao.java
+++ librodeesher-core/src/main/java/com/softwaremagico/persistence/dao/hibernate/GenericDao.java
@@ -8,12 +8,12 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Projections;
+import org.hibernate.transform.DistinctRootEntityResultTransformer;
 
 import com.softwaremagico.persistence.HibernateInitializator;
 import com.softwaremagico.persistence.dao.IGenericDao;
 
-public abstract class GenericDao<T> extends StorableObjectDao<T> implements
-		IGenericDao<T> {
+public abstract class GenericDao<T> extends StorableObjectDao<T> implements IGenericDao<T> {
 
 	private Class<T> type;
 
@@ -28,7 +28,7 @@ public abstract class GenericDao<T> extends StorableObjectDao<T> implements
 	}
 
 	protected SessionFactory getSessionFactory() {
-		if(sessionFactory==null){
+		if (sessionFactory == null) {
 			sessionFactory = HibernateInitializator.getSessionFactory();
 		}
 		return sessionFactory;
@@ -106,10 +106,16 @@ public abstract class GenericDao<T> extends StorableObjectDao<T> implements
 		Session session = getSessionFactory().getCurrentSession();
 		session.beginTransaction();
 		try {
+			// session.createCriteria(getType()).list() is not working returns repeated elements due to
+			// http://stackoverflow.com/questions/8758363/why-session-createcriteriaclasstype-list-return-more-object-than-in-list
+			// if we have a list with eager fetch.
+			Criteria criteria = session.createCriteria(getType());
+			// This is executed in java side.
+			criteria.setResultTransformer(DistinctRootEntityResultTransformer.INSTANCE);
+			criteria.addOrder(Order.desc("updateTime"));
 			@SuppressWarnings("unchecked")
-			List<T> objects = session.createCriteria(getType()).list();
+			List<T> objects = criteria.list();
 			initializeSets(objects);
-			session.createCriteria(type).addOrder(Order.desc("updateTime"));
 			session.getTransaction().commit();
 			return objects;
 		} catch (RuntimeException e) {
@@ -131,8 +137,8 @@ public abstract class GenericDao<T> extends StorableObjectDao<T> implements
 
 	/**
 	 * When using lazy loading, the sets must have a proxy to avoid a
-	 * "LazyInitializationException: failed to lazily initialize a collection of..."
-	 * error. This procedure must be called before closing the session.
+	 * "LazyInitializationException: failed to lazily initialize a collection of..." error. This procedure must be
+	 * called before closing the session.
 	 * 
 	 * @param planningEvent
 	 */
@@ -144,8 +150,8 @@ public abstract class GenericDao<T> extends StorableObjectDao<T> implements
 
 	/**
 	 * When using lazy loading, the sets must have a proxy to avoid a
-	 * "LazyInitializationException: failed to lazily initialize a collection of..."
-	 * error. This procedure must be called before closing the session.
+	 * "LazyInitializationException: failed to lazily initialize a collection of..." error. This procedure must be
+	 * called before closing the session.
 	 * 
 	 * @param elements
 	 */
