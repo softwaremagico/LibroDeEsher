bd3c41ec23e0db7dd8914ffe687d85687cbc6d11
Jorge Hortelano <jhortelano@biit-solutions.com>
2014-08-22 11:51:29 +0200

    Deprecated hiberante function replaced.

--- librodeesher-core/src/main/java/com/softwaremagico/persistence/HibernateInitializator.java
+++ librodeesher-core/src/main/java/com/softwaremagico/persistence/HibernateInitializator.java
@@ -1,9 +1,9 @@
 package com.softwaremagico.persistence;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
-import org.hibernate.service.ServiceRegistryBuilder;
 
 public class HibernateInitializator {
 
@@ -13,8 +13,8 @@ public class HibernateInitializator {
 		try {
 			Configuration configuration = new Configuration();
 			configuration.configure();
-			ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties())
-					.buildServiceRegistry();
+			ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
+					configuration.getProperties()).build();
 			sessionFactory = configuration.buildSessionFactory(serviceRegistry);
 			return sessionFactory;
 		} catch (Throwable ex) {
