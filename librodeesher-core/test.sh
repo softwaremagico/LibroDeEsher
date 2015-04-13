848e8d3f184417644f4958d8af272d739ca0bfc8
Jorge Hortelano <jhortelano@biit-solutions.com>
2015-04-13 10:48:13 +0200

    Test battery improved.

--- librodeesher-core/test.sh
+++ librodeesher-core/test.sh
@@ -6,8 +6,7 @@ for i in `seq 1 100`
 do
 	folder="tests/test-$i";
 	mkdir -p $folder >/dev/null;
-	#mvn test > $folder/ouput.txt;
-	mvn test | awk '/^\[ERROR\]/ { print  > "error.log"; next; }; 1' > $folder/ouput.txt;
+	mvn test > $folder/ouput.out;
 	mv /tmp/characterAsJson.txt $folder;
 	mv /tmp/importedCharacterSheet.txt $folder;
 	mv /tmp/importedLevelSheet.txt $folder;
@@ -16,4 +15,7 @@ do
 	mv /tmp/testLevelJson.txt $folder;
 	mv /tmp/testStandard.txt $folder;	
 done;
+
+#Search for errors.
+grep --include=\*.out -rnw 'tests' -e "Tests run: 15, Failures: 1" > error.log
 
