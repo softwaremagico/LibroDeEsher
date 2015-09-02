5a9b9c8690fd9011fc9089fc5f7edbc00a2e5016
Jorge Hortelano <softwaremagico@gmail.com>
2015-09-02 21:05:13 +0200

    Tests improved.

--- librodeesher-core/test.sh
+++ librodeesher-core/test.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+GLOBAL_STARTTIME=$(date +%s)
 rm -r tests 2> /dev/null;
 rm errors.log 2> /dev/null;
 for i in `seq 1 100`
 do
+	STARTTIME=$(date +%s)
+	echo "---- TEST $i ----"
 	folder="tests/test-$i";
 	mkdir -p $folder >/dev/null;
 	mvn test > $folder/ouput.out;
@@ -13,9 +16,14 @@ do
 	mv /tmp/originalCharacterSheet.txt $folder;
 	mv /tmp/originalSheet.txt $folder;
 	mv /tmp/testLevelJson.json $folder;
-	mv /tmp/testStandard.txt $folder;	
+	mv /tmp/testStandard.txt $folder;
+	mv /tmp/importedMorticia.txt $folder;
+	ENDTIME=$(date +%s)
+	echo "Duration $(($ENDTIME - $STARTTIME)) seconds."
 done;
+GLOBAL_ENDTIME=$(date +%s)
+echo "Tdtal Duration $(($GLOBAL_ENDTIME - $GLOBAL_STARTTIME)) seconds."
 
-#Search for errors.
-grep --include=\*.out -rnw 'tests' -e "Tests run: 15, Failures: 1" > error.log
+#Search for errors. -L for not contains "Failures: 0".
+grep --include=\*.out -rnwL 'tests' -e "Failures: 0" > error.log
 
