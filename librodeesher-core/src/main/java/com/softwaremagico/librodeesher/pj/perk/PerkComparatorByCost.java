2ee81bbfdae92ae5b043a9aa94a02cc0ca3f8d9d
Jorge Hortelano <jhortelano@biit-solutions.com>
2016-06-11 19:29:46 +0200

    Perks can be sorted by cost

--- librodeesher-core/src/main/java/com/softwaremagico/librodeesher/pj/perk/PerkComparatorByCost.java
+++ librodeesher-core/src/main/java/com/softwaremagico/librodeesher/pj/perk/PerkComparatorByCost.java
@@ -2,10 +2,13 @@ package com.softwaremagico.librodeesher.pj.perk;
 
 import java.util.Comparator;
 
-public class PerkComparator implements Comparator<Perk> {
+public class PerkComparatorByCost implements Comparator<Perk> {
 
 	@Override
 	public int compare(Perk o1, Perk o2) {
+		if (o1.getCost() != o2.getCost()) {
+			return o1.getCost().compareTo(o2.getCost());
+		}
 		return o1.getName().compareTo(o2.getName());
 	}
 }
---
+++ librodeesher-core/src/main/java/com/softwaremagico/librodeesher/pj/perk/PerkComparatorByName.java
@@ -0,0 +1,11 @@
+package com.softwaremagico.librodeesher.pj.perk;
+
+import java.util.Comparator;
+
+public class PerkComparatorByName implements Comparator<Perk> {
+
+	@Override
+	public int compare(Perk o1, Perk o2) {
+		return o1.getName().compareTo(o2.getName());
+	}
+}
